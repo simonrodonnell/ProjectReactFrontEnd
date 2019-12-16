0164b62beda2592093d840aedb6bc3dab51e94d6
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-12-16 20:16:52 +0000

    Added remaining paddock js files and additional NavBar links

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,13 +6,19 @@ const NavBar = (props) => {
 
       <ul>
         <li className="navLink">
-          <a href="/dinosaurs">Dinosaurs</a>
+          <a href="/parks">Park Admin</a>
         </li>
         <li className="navLink">
-          <a href="/paddocks">Paddocks</a>
+          <a href="/dinosaurs">Dinosaur Admin</a>
+        </li>
+        <li className="navLink">
+          <a href="/paddocks">Paddock Admin</a>
         </li>
         <li className="navLink">
           <a href="/dinosaurs/new">Create Dinosaur</a>
+        </li>
+        <li className="navLink">
+          <a href="/paddocks/new">Create Paddock</a>
         </li>
 
       </ul>
