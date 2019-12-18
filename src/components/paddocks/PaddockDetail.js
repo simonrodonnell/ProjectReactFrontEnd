9cc6d0af91a91310e0638c8c78a70ee24c88f963
Andrew Wood <ajwood2001@gmail.com>
2019-12-18 19:31:39 +0000

    feed all dinosaurs working

--- src/components/paddocks/PaddockDetail.js
+++ src/components/paddocks/PaddockDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Paddock from './Paddock'
 import {Link} from 'react-router-dom'
+import Request from '../../helpers/request'
 
 const PaddockDetail = (props) => {
 
@@ -20,6 +21,21 @@ const PaddockDetail = (props) => {
     props.onDelete(props.paddock.id)
   }
 
+
+  const handleFeedDinos = () => {
+    const request = new Request();
+    props.paddock._embedded.dinosaurs.forEach((dinosaur) => {
+      dinosaur.isFed = true;
+      const id = dinosaur.id
+      const dinosaurUpdate = {
+        "isFed": true
+      }
+      request.patch('http://localhost:8080/api/dinosaurs/' + id, dinosaurUpdate)
+      console.log("Dino ID:", id)
+    })
+    console.log("Dinos:", props.paddock._embedded.dinosaurs)
+  }
+
   const editUrl = "/paddocks/" + props.paddock.id + "/edit";
   // const transferUrl = "/paddocks/" + props.paddock.id + "/transfer";
   // </Link>
@@ -34,6 +50,7 @@ const PaddockDetail = (props) => {
     <ul>
     {dinosaurs}
     </ul>
+    <button onClick={handleFeedDinos}>Feed All Dinosaurs</button>
     <button onClick={handleDelete}>Delete {props.paddock.name}</button>
     <Link to={editUrl}><button type="button">Edit {props.paddock.name}</button></Link>
     </div>
